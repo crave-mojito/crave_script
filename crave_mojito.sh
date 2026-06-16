b99c8113758a6dc2158a608acb79ebea12939767
K4LCHAKRA <k4lchakra@gmail.com>
2026-06-16 20:56:52 +0545

    Update repo init commands in crave_mojito.sh

--- crave_mojito.sh
+++ crave_mojito.sh
@@ -1,6 +1,6 @@
 # repo init -u https://github.com/LineageOS/android.git -b lineage-23.0 --git-lfs
-repo init --depth=1 -u https://github.com/ProjectMatrixx/android -b 16.2 --git-lfs
-# repo init --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 16 -g default,-mips,-darwin,-notdefault
+# repo init --depth=1 -u https://github.com/ProjectMatrixx/android -b 16.2 --git-lfs
+repo init --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 16 -g default,-mips,-darwin,-notdefault
 # repo init -u https://github.com/RisingOS-Revived/android -b sixteen --git-lfs
 /opt/crave/resync.sh
 git clone https://github.com/Infinity-X-Devices/android_device_xiaomi_mojito.git -b 16.0 device/xiaomi/mojito
