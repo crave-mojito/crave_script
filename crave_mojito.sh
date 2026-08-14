87e072d395beedb25229ef868d0964f6f42fb89e
K4LCHAKRA <k4lchakra@gmail.com>
2026-08-14 16:13:29 +0545

    Update crave_mojito.sh

--- crave_mojito.sh
+++ crave_mojito.sh
@@ -1,19 +1,19 @@
 # repo init -u https://github.com/LineageOS/android.git -b lineage-23.0 --git-lfs
 # repo init --depth=1 -u https://github.com/ProjectMatrixx/android -b 16.2 --git-lfs
-# repo init --depth=1 --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 16 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 16 -g default,-mips,-darwin,-notdefault
 # repo init -u https://github.com/RisingOS-Revived/android -b sixteen --git-lfs
-repo init --depth=1 -u https://github.com/VoltageOS/manifest.git -b 17 --git-lfs
+# repo init --depth=1 -u https://github.com/VoltageOS/manifest.git -b 17 --git-lfs
 # repo sync
 /opt/crave/resync.sh
-# git clone https://github.com/Infinity-X-Devices/android_device_xiaomi_mojito.git -b 16.0 device/xiaomi/mojito
-# git clone https://github.com/Infinity-X-Devices/device_xiaomi_sm6150-common.git -b 16.0 device/xiaomi/sm6150-common
-# git clone https://github.com/Infinity-X-Devices/kernel_xiaomi_mojito --depth 1 -b inline-rorn kernel/xiaomi/mojito
-# git clone https://gitlab.com/mojito22/android_vendor_xiaomi_mojito.git -b qpr vendor/xiaomi/mojito
-# git clone https://gitlab.com/mojito22/android_vendor_xiaomi_sm6150-common.git -b 16.0 vendor/xiaomi/sm6150-common
-# git clone https://github.com/BlissRoms-Devices/android_hardware_xiaomi.git --depth 1 -b mojito-rom hardware/xiaomi
-# . build/envsetup.sh
-# lunch lineage-mojito-user
-# bacon
+git clone https://github.com/Infinity-X-Devices/android_device_xiaomi_mojito.git -b 16.0 device/xiaomi/mojito
+git clone https://github.com/Infinity-X-Devices/device_xiaomi_sm6150-common.git -b 16.0 device/xiaomi/sm6150-common
+git clone https://github.com/Infinity-X-Devices/kernel_xiaomi_mojito --depth 1 -b inline-rorn kernel/xiaomi/mojito
+git clone https://gitlab.com/mojito22/android_vendor_xiaomi_mojito.git -b qpr vendor/xiaomi/mojito
+git clone https://gitlab.com/mojito22/android_vendor_xiaomi_sm6150-common.git -b 16.0 vendor/xiaomi/sm6150-common
+git clone https://github.com/BlissRoms-Devices/android_hardware_xiaomi.git --depth 1 -b mojito-rom hardware/xiaomi
+. build/envsetup.sh
+lunch lineage-mojito-user
+bacon
 
 # repo init --depth=1 -u https://github.com/Pixelify-AOSP/platform_manifest -b 17 --git-lfs
 # /opt/crave/resync.sh
@@ -25,24 +25,25 @@ repo init --depth=1 -u https://github.com/VoltageOS/manifest.git -b 17 --git-lfs
 # git clone https://gitlab.com/mojito22/android_vendor_xiaomi_mojito.git -b qpr1 vendor/xiaomi/mojito
 # git clone https://gitlab.com/mojito22/android_vendor_xiaomi_sm6150-common.git -b 16 vendor/xiaomi/sm6150-common
 # git clone https://github.com/BlissRoms-Devices/android_hardware_xiaomi.git --depth 1 -b mojito hardware/xiaomi
-rm -rf device/xiaomi/mojito
-rm -rf device/xiaomi/sm6150-common
+# rm -rf device/xiaomi/mojito
+# 
+# rm -rf device/xiaomi/sm6150-common
 # rm -rf kernel/xiaomi/mojito
-rm -rf vendor/xiaomi/mojito
-rm -rf vendor/xiaomi/sm6150-common
-rm -rf mojito hardware/xiaomi
+# rm -rf vendor/xiaomi/mojito
+# rm -rf vendor/xiaomi/sm6150-common
+# rm -rf mojito hardware/xiaomi
 
-git clone https://github.com/Infinity-X-Devices/device_xiaomi_mojito.git -b 17-vos device/xiaomi/mojito
-git clone https://github.com/Infinity-X-Devices/device_xiaomi_sm6150-common.git -b 17-vos device/xiaomi/sm6150-common
+# git clone https://github.com/Infinity-X-Devices/device_xiaomi_mojito.git -b 17-vos device/xiaomi/mojito
+# git clone https://github.com/Infinity-X-Devices/device_xiaomi_sm6150-common.git -b 17-vos device/xiaomi/sm6150-common
 # git clone https://github.com/Infinity-X-Devices/device_xiaomi_sm6150-common.git -b 15-QPR2-los-FN-5.4-5.10-CK kernel/xiaomi/mojito
 
-git clone https://gitlab.com/mojito22/android_vendor_xiaomi_mojito.git -b qpr1 vendor/xiaomi/mojito
-git clone https://gitlab.com/mojito22/android_vendor_xiaomi_sm6150-common.git -b 16 vendor/xiaomi/sm6150-common
-rm -rf hardware/xiaomi
-git clone https://github.com/BlissRoms-Devices/android_hardware_xiaomi.git --depth 1 -b mojito hardware/xiaomi
+# git clone https://gitlab.com/mojito22/android_vendor_xiaomi_mojito.git -b qpr1 vendor/xiaomi/mojito
+# git clone https://gitlab.com/mojito22/android_vendor_xiaomi_sm6150-common.git -b 16 vendor/xiaomi/sm6150-common
+# rm -rf hardware/xiaomi
+# git clone https://github.com/BlissRoms-Devices/android_hardware_xiaomi.git --depth 1 -b mojito hardware/xiaomi
 
-rm -rf vendor/voltage-priv/keys
-git clone https://github.com/mojito-keys/vendor_voltage-priv_keys.git -b main vendor/voltage-priv/keys
+# rm -rf vendor/voltage-priv/keys
+# git clone https://github.com/mojito-keys/vendor_voltage-priv_keys.git -b main vendor/voltage-priv/keys
 
 # . build/envsetup.sh
 # lunch custom_mojito-cp2a-userdebug
@@ -51,5 +52,5 @@ git clone https://github.com/mojito-keys/vendor_voltage-priv_keys.git -b main ve
 # mka kernel
 # mka precompiled_sepolicy
 
-. build/envsetup.sh
-brunch mojito
+# . build/envsetup.sh
+# brunch mojito
